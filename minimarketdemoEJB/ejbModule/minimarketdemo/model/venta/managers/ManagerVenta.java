19bc317b183f5586617f882eebda98f6dcf12512
Users <Users@DESKTOP-7P9DGOI>
2021-03-18 16:15:03 -0500

    commit venta proyecto

---
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/venta/managers/ManagerVenta.java
@@ -0,0 +1,56 @@
+package minimarketdemo.model.venta.managers;
+
+
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import minimarketdemo.model.core.entities.FactDetalle;
+import minimarketdemo.model.core.entities.ThmEmpleado;
+import minimarketdemo.model.core.entities.VentRegistro;
+import minimarketdemo.model.core.managers.ManagerDAO;
+
+/**
+ * Session Bean implementation class ManagerAuditoria
+ */
+@Stateless
+@LocalBean
+public class ManagerVenta {
+	@EJB
+	private ManagerDAO mDAO;
+    /**
+     * Default constructor. 
+     */
+    public ManagerVenta() {
+        
+    }
+    /**
+     * Metodo basico para mostrar mensajes de depuracion.
+     * @param clase Informacion de la clase que se esta depurando.
+     * @param nombreMetodo Metodo que genera el mensaje para depuracion.
+     * @param mensaje El mensaje a desplegar.
+     */
+    //Ventas registro:
+    public List<VentRegistro> findAllVenta(){
+    	return mDAO.findAll(VentRegistro.class);
+    }
+    ///empleado
+    public ThmEmpleado findByIdThmEmpleado(int idThmEmpleado) throws Exception {
+    	return (ThmEmpleado) mDAO.findById(ThmEmpleado.class, idThmEmpleado);
+    }
+    public List<ThmEmpleado> findAllThmEmpleado(){
+    	return mDAO.findAll(ThmEmpleado.class, "horasTrabajadas");
+    }
+    ///fac_detalle
+    public FactDetalle findByidFactDetalle(int idFactDetalle) throws Exception{
+    	return (FactDetalle)mDAO.findById(FactDetalle.class, idFactDetalle);
+    }
+    public List<FactDetalle> findAllFactDetalle(){
+    	return mDAO.findAll(FactDetalle.class, "costoUnitarioDetalle");
+    }
+    
+
+}
---
+++ minimarketdemoWeb/src/minimarketdemo/controller/venta/BeanVenta.java
@@ -0,0 +1,98 @@
+package minimarketdemo.controller.venta;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import minimarketdemo.model.core.entities.FactDetalle;
+import minimarketdemo.model.core.entities.SegUsuario;
+import minimarketdemo.model.core.entities.ThmEmpleado;
+import minimarketdemo.model.core.entities.VentRegistro;
+import minimarketdemo.model.seguridades.managers.ManagerSeguridades;
+import minimarketdemo.model.venta.managers.ManagerVenta;
+
+@Named
+@SessionScoped
+public class BeanVenta implements Serializable {
+	@EJB
+	private ManagerVenta managerVenta;
+	@EJB
+	private ManagerSeguridades mSeguridades;
+	private VentRegistro nuevoVenta;
+	private List<VentRegistro> listaVenta;
+	private List<ThmEmpleado> listaEmpleados;
+	private List<SegUsuario> listaUsuarios;
+	private List<FactDetalle> listafactdetalle;
+	private int idSegUsuario;
+	private int idFactDetalle;
+
+	public BeanVenta() {
+
+	}
+
+	@PostConstruct
+	public void inicializacion() {
+		System.out.println("BeanVentas inicializado...");
+		nuevoVenta = new VentRegistro();
+	}
+	///me falta factdetalle/////
+	public String actionCargarMenuVentas() {
+		listaUsuarios=mSeguridades.findAllUsuarios();
+		listaVenta = managerVenta.findAllVenta();
+		return "ventas?faces-redirect=true";
+	}
+
+	public VentRegistro getNuevoVenta() {
+		return nuevoVenta;
+	}
+
+	public void setNuevoVenta(VentRegistro nuevoVenta) {
+		this.nuevoVenta = nuevoVenta;
+	}
+
+	public List<VentRegistro> getListaVenta() {
+		return listaVenta;
+	}
+
+	public void setListaVenta(List<VentRegistro> listaVenta) {
+		this.listaVenta = listaVenta;
+	}
+
+	public List<ThmEmpleado> getListaEmpleados() {
+		return listaEmpleados;
+	}
+
+	public void setListaEmpleados(List<ThmEmpleado> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}
+
+	public List<SegUsuario> getListaUsuarios() {
+		return listaUsuarios;
+	}
+
+	public void setListaUsuarios(List<SegUsuario> listaUsuarios) {
+		this.listaUsuarios = listaUsuarios;
+	}
+
+	public int getIdSegUsuario() {
+		return idSegUsuario;
+	}
+
+	public void setIdSegUsuario(int idSegUsuario) {
+		this.idSegUsuario = idSegUsuario;
+	}
+
+	public int getIdFactDetalle() {
+		return idFactDetalle;
+	}
+
+	public void setIdFactDetalle(int idFactDetalle) {
+		this.idFactDetalle = idFactDetalle;
+	}
+
+}
