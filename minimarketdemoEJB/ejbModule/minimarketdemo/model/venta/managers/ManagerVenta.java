12afca59be950c7e477543055538b1913568d21b
Users <Users@DESKTOP-7P9DGOI>
2021-03-18 20:55:11 -0500

    commit venta registro

--- minimarketdemoEJB/ejbModule/minimarketdemo/model/venta/managers/ManagerVenta.java
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/venta/managers/ManagerVenta.java
@@ -33,18 +33,18 @@ public class ManagerVenta {
      * @param nombreMetodo Metodo que genera el mensaje para depuracion.
      * @param mensaje El mensaje a desplegar.
      */
-    //Ventas registros:
+    //Ventas registros:...
     public List<VentRegistro> findAllVenta(){
     	return mDAO.findAll(VentRegistro.class);
     }
-    ///thm_empleados:
+    ///thm_empleados:....
     public ThmEmpleado findByIdThmEmpleado(int idThmEmpleado) throws Exception {
     	return (ThmEmpleado) mDAO.findById(ThmEmpleado.class, idThmEmpleado);
     }
     public List<ThmEmpleado> findAllThmEmpleado(){
     	return mDAO.findAll(ThmEmpleado.class, "horasTrabajadas");
     }
-    ///factura_detalles:
+    ///factura_detalles:....
     public FactDetalle findByidFactDetalle(int idFactDetalle) throws Exception{
     	return (FactDetalle)mDAO.findById(FactDetalle.class, idFactDetalle);
     }
--- minimarketdemoWeb/src/minimarketdemo/controller/venta/BeanVenta.java
+++ minimarketdemoWeb/src/minimarketdemo/controller/venta/BeanVenta.java
@@ -40,7 +40,7 @@ public class BeanVenta implements Serializable {
 		System.out.println("BeanVentas inicializado...");
 		nuevoVenta = new VentRegistro();
 	}
-	/// falta factdetalle utilizado/////
+	/// falta factdetalle utilizado://///
 	public String actionCargarMenuVentas() {
 		listaUsuarios=mSeguridades.findAllUsuarios();
 		listafactdetalle =managerVenta.findAllFactDetalle();
